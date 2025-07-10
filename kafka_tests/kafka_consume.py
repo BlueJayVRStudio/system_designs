87dd3d50942a9806aae92a88e3651560d666249d
BlueJayVRStudio <BlueJayVRStudio@gmail.com>
2025-07-10 17:01:49 -0500

    update

--- kafka_tests/kafka_consume.py
+++ kafka_tests/kafka_consume.py
@@ -4,7 +4,7 @@ import os
 
 conf = {
     'bootstrap.servers': 'localhost:9092',
-    'group.id': 'debug-consumer-group-1',  # Keep same across all consumers
+    'group.id': 'debug-consumer-group-1',  
     'enable.auto.commit': False,
     'auto.offset.reset': 'earliest'
 }
@@ -20,17 +20,20 @@ print(f"[PID {os.getpid()}] Waiting for messages...")
 
 try:
     while True:
-        msg = consumer.poll(1.0)
-        if msg is None:
-            continue
-        if msg.error():
-            print(f"Error: {msg.error()}")
-            continue
+        # msg = consumer.poll(1.0)
+        messages = consumer.consume(num_messages=100)
 
-        print(f"[PID {os.getpid()}] Received from partition {msg.partition()}: {msg.value().decode('utf-8')}")
-        time.sleep(10)  # Simulate processing delay
-        consumer.commit(msg)
+        for msg in messages:
+            if msg is None:
+                continue
+            if msg.error():
+                print(f"Error: {msg.error()}")
+                continue
 
+            print(f"[PID {os.getpid()}] Received from partition {msg.partition()}: {msg.value().decode('utf-8')}")
+            # time.sleep(10)  # Simulate processing delay
+        if messages:
+            consumer.commit(asynchronous=True)
 except KeyboardInterrupt:
     print("Exiting")
 finally:
--- kafka_tests/kafka_produce.py
+++ kafka_tests/kafka_produce.py
@@ -1,17 +1,50 @@
 from confluent_kafka import Producer
 import time
-import uuid
+import random
 
+def delivery_report(err, msg):
+    if err:
+        print(f"Delivery failed: {err}")
+    # else:
+    #     print(f"Delivered to {msg.topic()} [{msg.partition()}]")
 
 p = Producer({
-    'bootstrap.servers': 'localhost:9092'
+    'bootstrap.servers': 'localhost:9092',
+    'linger.ms': 5,
+    'batch.num.messages': 1000
 })
 
-for i in range(3):
-    # key = f"user-{i}"
-    # key = str(i)
-    key = str(uuid.uuid4())  
+message = '0' * 100
 
-    p.produce('my-topic', key = key.encode(), value=f"msg-{i}".encode())
-    p.flush()
-    time.sleep(0.2)
+t0 = time.perf_counter()
+
+# for j in range(10000):
+for j in range(10000):
+    for i in range(1000):
+        key = str(random.randint(0, 100))
+        p.produce(
+            'my-topic',
+            key=key.encode(),
+            value=(f"msg-{key}" + message).encode(),
+            callback=delivery_report
+        )
+        p.poll(0)  # Handle events like acks
+    
+    if j % 20 == 0:
+        p.flush()  # Wait for all messages to be sent
+    time.sleep(0.001)
+
+message = '1' * 100
+for j in range(1):
+    for i in range(1000):
+        key = str(random.randint(0, 100))
+        p.produce(
+            'my-topic',
+            key=key.encode(),
+            value=(f"msg-{key}" + message).encode(),
+            callback=delivery_report
+        )
+        p.poll(0)  # Handle events like acks
+    p.flush()  # Wait for all messages to be sent
+
+print(f"job done, took: {time.perf_counter() - t0} seconds")
